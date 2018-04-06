6eb60032d33741cd62d069ac990606bb71f6ca71
MarkWieczorek <mark.wieczorek@oca.eu>
2018-04-06 19:21:48 +0200

    Add function for calculating the hydrostatic relief in internal density interfaces.

---
+++ Core-Moon.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+"""
+    Calculate the hydrostatic shape of the core of the Moon.
+
+"""
+import numpy as np
+
+import pyshtools
+
+from HydrostaticShapeLith import HydrostaticFlatteningLith
+
+# ==== MAIN FUNCTION ====
+
+
+def main():
+
+    lmax_grid = 359
+    lmax = 6
+
+    omega = float(pyshtools.constant.omega_moon)
+    rem = float(pyshtools.constant.a_moon)
+    mass_earth = float(pyshtools.constant.mass_earth)
+    r0 = float(pyshtools.constant.r_moon)
+
+    cthick = 34.e3  # 43.e3 or 34.0e3
+    rho_crust = 2550.
+
+    out_rc_fc = "rc_fc_34_2550.dat"
+    out_rc_rhoc = "rc_rhoc_34_2550.dat"
+    out_rc_beta = "rc_beta_34_2550.dat"
+    sh_core = "core_34.sh"
+    core_shape = "core_shape_330_34.dat"
+
+    rcore_int = 1.e3
+    rcore_start = 250.e3
+    rcore_end = 450.e3
+
+    rhocore_start = 5000.
+    rhocore_end = 8000.
+    rhocore_int = 1.
+
+    ismr2 = 0.3927280  # Williams et al. (2014)
+    ismr2 = ismr2 * (1738.e3 / r0)**2
+
+    pot_file = "/Users/lunokhod/Moon/GRAIL/GravityModels/" + \
+        "JGGRAIL_900C11A_SHA.TAB"
+
+    coeffs, header, lmaxp = pyshtools.shio.shread(pot_file, lmax=10,
+                                                  header=True)
+    r0_pot = float(header[0])*1.e3
+    gm = float(header[1])*1.e9
+
+    potential = pyshtools.SHCoeffs.from_array(coeffs)
+    potential.r0 = r0_pot
+    potential.gm = gm
+
+    print("Mean planetary radius (km) = {:e}".format(r0 / 1.e3))
+    print("Is/MR2 (solid Moon using mean radius) = {:e}".format(ismr2))
+    print("Lmax of Gravitational potential = {:d}".format(lmaxp))
+    print("Reference radius of potential model (km) = {:e}"
+          .format(r0_pot/1.e3))
+    print("GM = {:e}".format(gm))
+    mass = gm / float(pyshtools.constant.grav_constant)
+    print("Mass (kg) = {:e}".format(mass))
+    print("Omega = {:e}".format(omega))
+    print("Period (days) = {:e}".format(2. * np.pi / omega / 60. / 60. / 24.))
+    print("Average crustal thickness (km) = {:e}".format(cthick / 1.e3))
+    print("Crustal density (kg/m3) = {:e}".format(rho_crust))
+
+    radius = np.zeros(4)
+    radius[0] = 0.
+    radius[2] = r0 - cthick
+    radius[3] = r0
+
+    rho = np.zeros(4)
+    rho[2] = rho_crust
+
+    mass_crust = 4. * np.pi / 3. * rho_crust * (radius[3]**3 - radius[2]**3)
+
+    n = 3
+    i_lith = 2
+    i_core = 1
+
+    # For each core radius, find rho_mantle and rho_core that fit total mass
+    # and moment of inertia
+
+    f_rc_fc = open(out_rc_fc, 'w')
+    f_rc_rhoc = open(out_rc_rhoc, 'w')
+    f_rc_beta = open(out_rc_beta, 'w')
+
+    for r_core in np.arange(rcore_start, rcore_end + rcore_int, rcore_int,
+                            dtype=float):
+        radius[1] = r_core
+        first = True
+
+        for rho_core in np.arange(rhocore_start, rhocore_end + rhocore_int,
+                                  rhocore_int, dtype=float):
+            mass_core = 4. * np.pi / 3. * rho_core * r_core**3
+            mass_mantle = mass - mass_crust - mass_core
+            rho_mantle = mass_mantle * 3. / np.pi / 4. / (radius[2]**3 -
+                                                          r_core**3)
+            rho[0] = rho_core
+            rho[1] = rho_mantle
+
+            if rho_mantle >= rho_core:
+                continue
+
+            ismr2_model = moi_solid(radius, rho, n)
+            if first is True:
+                diff_old = ismr2 - ismr2_model
+                first = False
+            else:
+                diff_new = ismr2 - ismr2_model
+
+                if diff_new * diff_old <= 0.:
+                    # interpolate to get the best fitting core density
+                    rho_core_final = (rho_core - rhocore_int) - diff_old * \
+                        (rhocore_int) / (diff_new - diff_old)
+                    rho[0] = rho_core_final
+                    mass_core = 4. * np.pi / 3. * rho[0] * r_core**3
+                    mass_mantle = mass - mass_crust - mass_core
+                    rho_mantle = mass_mantle * 3. / np.pi / 4. / \
+                        (radius[2]**3 - r_core**3)
+                    rho[1] = rho_mantle
+
+                    hlm, clm_hydro, mass_model = \
+                        HydrostaticFlatteningLith(radius, rho, i_lith,
+                                                  potential, omega, lmax,
+                                                  finiteamplitude=False,
+                                                  rp=rem, mp=mass_earth)
+
+                    a = hlm[1].expand(lat=0., lon=0., lmax_calc=lmax)
+                    b = hlm[1].expand(lat=0., lon=90., lmax_calc=lmax)
+                    c = hlm[1].expand(lat=90., lon=0., lmax_calc=lmax)
+
+                    f_core = ((a+b)/2. - c) / ((a + b) / 2.)
+                    beta_core = (a**2 - b**2) / (a**2 + b**2)
+
+                    print(r_core/1.e3, rho[0], rho[1], f_core, beta_core)
+                    f_rc_fc.write('{:e}, {:e}\n'.format(r_core/1.e3, f_core))
+                    f_rc_rhoc.write('{:e}, {:e}\n'.format(r_core/1.e3, rho[0]))
+                    f_rc_beta.write('{:e}, {:e}\n'
+                                    .format(r_core/1.e3, beta_core))
+
+                    if r_core == 330.e3:
+                        hlm[i_core].to_file(sh_core)
+
+                        print("Rcore (km) = {:e}".format(r_core/1.e3))
+                        print("A (km) = {:e}".format(a/1.e3))
+                        print("B (km) = {:e}".format(b/1.e3))
+                        print("C (km) = {:e}".format(c/1.e3))
+                        print("rho_core (kg/m3) = {:e}".format(rho[0]))
+
+                        grid = hlm[i_core].expand(lmax=lmax_grid, grid='DH2')
+                        grid.to_file(core_shape)
+                        print("Size of output grid = {:d}, {:d}"
+                              .format(grid.nlat, grid.nlon))
+                        print("Maximum = {:e}\nMinimum = {:e}"
+                              .format(grid.data.max(), grid.data.min()))
+
+                diff_old = diff_new
+
+
+def moi_solid(radius, rho, n):
+    """
+    Calculate the mean, normalized, moment of inertia of the solid portion
+    of the planet.
+
+    The radius and density are discretized into shells as in the hydrostatic
+    flattening routines:
+        radius[0] = 0
+        radius[1] = radius of core
+        radius[n] = surface
+        rho[i] = density from radius[i] to radius[i+1]
+    """
+    moi_solid = 0.
+    mass = 4. * np.pi / 3. * rho[0] * radius[1]**3
+
+    for i in range(2, n+1):
+        mass += 4. * np.pi / 3. * rho[i-1] * (radius[i]**3 - radius[i-1]**3)
+
+        moi_solid += 8. * np.pi / 15. * rho[i-1] * (radius[i]**5 -
+                                                    radius[i-1]**5)
+
+    return moi_solid / mass / radius[n]**2
+
+
+# ==== EXECUTE SCRIPT ====
+
+
+if __name__ == "__main__":
+    main()
---
+++ HydrostaticShapeLith.py
@@ -0,0 +1,364 @@
+'''
+Functions for calculating the shape of hydrostatic density interfaces and their
+gravitational potential in a planet with a non-hydrostatic lithosphere.
+
+    HydrostaticFlatteningLith
+'''
+import numpy as np
+import scipy.linalg.lapack as lapack
+
+import pyshtools as pyshtools
+
+
+# ==== HydrostaticFlatteningLith ====
+
+def HydrostaticFlatteningLith(radius, rho, ilith, potential, omega, lmax,
+                              finiteamplitude=False, rp=None, mp=None):
+    """
+    Calculate the shape of hydrostatic relief in a rotating planet or moon with
+    a non-hydrostatic lithosphere, along with the total gravitation potential
+    of the hydrostatic interfaces. For the case of a moon in synchronous
+    rotation, optionally include the tidal potential.
+
+    Usage
+    -----
+    hlm, clm_hydro, mass = HydrostaticFlatteningLith(radius, density, ilith,
+        potential, omega, lmax, [finiteamplitude, rp])
+
+    Returns
+    -------
+    hlm : array of SHCoeffs class instances, size(n+1)
+        Array of SHCoeffs class instances of the spherical harmonic
+        coefficients of the hydrostatic relief at each interface.
+    clm_hydro : SHCoeffs class instance containing the gravitational potential
+        resulting from all hydrostatic interfaces.
+    mass : float
+        Total mass of the planet, assuming a spherical shape and the provided
+        1D density profile.
+
+    Parameters
+    ----------
+    radius : ndarray, float, size(n+1)
+        Radius of each density interface, where index 0 corresponds to the
+        center of the planet and n corresponds to the surface.
+    density : ndarray, float, size(n+1)
+        Density of layer i between radius[i] and radius[i+1]. The density
+        at index 0 is from the center of the planet to radius[1], whereas the
+        the density at index n should be zero.
+    ilith : int
+        Index of the interface that corresponds to the base of the lithosphere.
+    potential : SHCoeffs class instance
+        Observed gravitational potential coefficients. The class instance must
+        set the attributes gm and r0 for the GM and reference radius of the
+        coefficients, respectively.
+    omega : float
+        Angular rotation rate of the planet.
+    lmax : int
+        Maximum spherical harmonic degree to compute for the hydrostatic
+        relief at each interface.
+    finiteamplitude : bool, optional, default = False
+        If True, compute finite amplitude terms when calculating the
+        gravitational potentials.
+    rp : optional, default = None
+        If specified, include the tidal potential acting on a synchronously
+        rotating moon, where rp is the average distance between the planet
+        and satellite.
+    mp : optional, default = None
+        The mass of the host planet, at a distance rp from the satellite.
+    """
+    tides = False
+    if rp is not None:
+        if mp is None:
+            raise ValueError('When including tides, both rp and mp must be ' +
+                             'specified.')
+        tides = True
+    if mp is not None:
+        if rp is None:
+            raise ValueError('When including tides, both rp and mp must be ' +
+                             'specified.')
+        tides = True
+
+    if len(radius) != len(rho):
+        raise('Length of radius and density must be the same.' +
+              'len(radius) = {:d}. len(density) = {:d}.'
+              .format(len(radius), len(rho)))
+
+    n = len(radius) - 1  # index of surface
+    lmaxgrid = 2*lmax  # increase grid size to avoid aliasing
+
+    g = float(pyshtools.constant.grav_constant)
+    gm = potential.gm
+    r0 = potential.r0
+
+    hlm = [pyshtools.SHCoeffs.from_zeros(lmax) for i in range(ilith+1)]
+    clm_hydro = pyshtools.SHCoeffs.from_zeros(lmax)
+
+    for i in range(ilith+1):
+        hlm[i].coeffs[0, 0, 0] = radius[i]
+
+    if finiteamplitude:
+        nmax = 6
+        kmax = 4
+        dcminus = np.zeros((ilith+1, 2, lmax+1, lmax+1))
+        dcplus = np.zeros((ilith+1, 2, lmax+1, lmax+1))
+    else:
+        kmax = 1
+        nmax = 1
+
+    # First determine the spherical harmonic coefficients of (Y20 Ylm)
+    # and for tides, (Y22 Ylm). We are only concerned with the coefficient
+    # that corresponds to lm, so for each lm, store only the lm component in
+    # the array cp20 and cp22.
+
+    sh20 = np.zeros((2, lmax+1, lmax+1))
+    sh22 = np.zeros((2, lmax+1, lmax+1))
+    sh = np.zeros((2, lmax+1, lmax+1))
+    cp20 = np.zeros((2, lmax+1, lmax+1))
+    cp22 = np.zeros((2, lmax+1, lmax+1))
+
+    sh20[0, 2, 0] = 1.  # Y20
+    sh22[0, 2, 2] = 1.  # Y22
+
+    for l in range(2, lmax+1):
+        for m in range(0, l+1):
+            sh[0, l, m] = 1.
+            coeffs = pyshtools.expand.SHMultiply(sh20, sh)
+            cp20[0, l, m] = coeffs[0, l, m]
+            if m != 0:
+                cp20[1, l, m] = cp20[0, l, m]
+            if l == 2 and m == 0:
+                p402020 = coeffs[0, 4, 0]
+            if l == 2 and m == 1:
+                p412021 = coeffs[0, 4, 1]
+            if l == 2 and m == 2:
+                p422022 = coeffs[0, 4, 2]
+
+            coeffs = pyshtools.expand.SHMultiply(sh22, sh)
+            cp22[0, l, m] = coeffs[0, l, m]
+            sh[0, l, m] = 0.
+
+            if m > 0:
+                sh[1, l, m] = 1.
+                coeffs = pyshtools.expand.SHMultiply(sh22, sh)
+                cp22[1, l, m] = coeffs[1, l, m]
+                sh[1, l, m] = 0.
+
+    # Calculate mass(<=r), delta_rho
+
+    drho = np.zeros(n+1)
+    mass = np.zeros(n+1)
+
+    for i in range(1, n+1):
+        drho[i] = rho[i-1] - rho[i]
+        if i == 1:
+            mass[i] = 4. * np.pi * radius[1]**3 * rho[0] / 3.
+        else:
+            mass[i] = mass[i-1] + 4. * np.pi * \
+                (radius[i]**3 - radius[i-1]**3) * rho[i-1] / 3.
+
+    mass_model = mass[n]
+
+    # Calculate matrix A and invert for relief.
+
+    a = np.zeros((ilith+2, ilith+2))
+    atides = np.zeros((2, ilith+1))
+    b4 = np.zeros((2, 3, ilith+1))
+    mass_sheet = np.zeros((2, lmax+1, lmax+1))
+
+    for k in range(kmax):
+        # calculate finite amplitude corrections
+        if finiteamplitude and k > 0:
+            for i in range(1, ilith+1):
+                grid = hlm[i].expand(grid='DH1', lmax=lmaxgrid, lmax_calc=lmax)
+                clm, r = pyshtools.gravmag.CilmPlusDH(grid.data, nmax, gm/g,
+                                                      drho[i], lmax=lmax)
+                dcplus[i, :, :, :] = clm[:, :, :]
+                clm, r = pyshtools.gravmag.CilmMinusDH(grid.data, nmax, gm/g,
+                                                       drho[i], lmax=lmax)
+                dcminus[i, :, :, :] = clm[:, :, :]
+                for l in range(1, lmax+1):
+                    mass_sheet[:, l, :l+1] = 4. * np.pi * g * drho[i] * \
+                        radius[i]**2 * hlm[i].coeffs[:, l, :l+1] / gm / \
+                        (2. * l + 1.)
+                    dcplus[i, :, l, :l+1] -= mass_sheet[:, l, :l+1]
+                    dcminus[i, :, l, :l+1] -= mass_sheet[:, l, :l+1]
+
+        for l in range(2, lmax+1):
+            for m in range(0, lmax+1):
+                for i in range(1, ilith+1):  # zero index not computed
+                    for j in range(1, ilith+1):
+                        if i == j:  # cp20 for sin and cosine terms are equal
+                            a[i, j] = 4. * np.pi * g * drho[i] * radius[i] / \
+                                (2. * l + 1.) - g * mass[i] / radius[i]**2 + \
+                                (2./3.) * radius[i] * omega**2 * \
+                                (1. - cp20[0, l, m] / np.sqrt(5.0))
+                        elif j < i:
+                            a[i, j] = 4. * np.pi * g * drho[j] * \
+                                radius[j]**(l+2) / (2. * l + 1.) / \
+                                radius[i]**(l+1)
+                        else:
+                            a[i, j] = 4. * np.pi * g * drho[j] * \
+                                radius[i]**l / (2. * l + 1.) / \
+                                radius[j]**(l-1)
+
+                    a[i, ilith+1] = 4. * np.pi * g * radius[i]**l / \
+                        (2. * l + 1.) / radius[n]**(l-1)
+
+                    if tides is True:
+                        atides[0, i] = g * mp * radius[i] / rp**3 * (
+                            - np.sqrt(5.) / 5. * cp20[0, l, m] +
+                            np.sqrt(12./5.) * cp22[0, l, m] / 2.)
+                        atides[1, i] = g * mp * radius[i] / rp**3 * (
+                            - np.sqrt(5.) / 5. * cp20[1, l, m] +
+                            np.sqrt(12./5.) * cp22[1, l, m] / 2.)
+
+                for j in range(1, ilith+1):
+                    a[ilith+1, j] = 4. * np.pi * g * drho[j] * \
+                        radius[j]**(l+2) / (2. * l + 1.) / r0**(l+1)
+
+                a[ilith+1, ilith+1] = 4. * np.pi * g * radius[n]**(l+2) / \
+                    (2. * l + 1.) / r0**(l+1)
+
+                b = np.zeros(ilith+2)
+                if l == 2 and m == 0:
+                    for i in range(1, ilith+1):
+                        b[i] = (omega * radius[i])**2 / (3. * np.sqrt(5.))
+                        if tides:
+                            b[i] += g * mp * radius[i]**2 / rp**3 * \
+                                np.sqrt(5.) / 10.
+                if l == 2 and m == 2 and tides:
+                    for i in range(1, ilith+1):
+                        b[i] = - g * mp * radius[i]**2 / rp**3 * \
+                            np.sqrt(12./5.) / 4.
+
+                # --- do cosine term ---
+
+                # Add contributions from degree 2 relief to degree 4.
+                if l == 4 and m <= 2:
+                    b[1:ilith+1] = b4[0, m, 1:ilith+1]
+                b[ilith+1] = gm * potential.coeffs[0, l, m] / r0
+
+                # calculate delta
+                if k > 0:
+                    d = np.zeros(ilith+2)
+                    for i in range(1, ilith+1):
+                        for j in range(1, ilith+1):
+                            if j <= i:
+                                d[i] -= dcplus[j, 0, l, m] * gm * \
+                                    radius[j]**l / radius[i]**(l+1)
+                            else:
+                                d[i] -= dcminus[j, 0, l, m] * gm * \
+                                    radius[i]**l / radius[j]**(l+1)
+                    for j in range(1, ilith+1):
+                        d[ilith+1] -= dcplus[j, 0, l, m] * gm * \
+                                    radius[j]**l / r0**(l+1)
+
+                # solve the linear equation A h = b
+                atemp = a.copy()
+                if tides:
+                    for i in range(1, ilith+1):
+                        atemp[i, i] += atides[0, i]
+
+                btemp = b.copy()
+                if k > 0:
+                    btemp += d
+
+                # note that the zero index is not used
+                lu, piv, x, info = lapack.dgesv(atemp[1:, 1:], btemp[1:])
+                if info != 0:
+                    raise("lapack.dgesv did not exit properly: {:d}", info)
+                for i in range(1, ilith+1):
+                    hlm[i].coeffs[0, l, m] = x[i-1]
+
+                # calculate b4 contribution
+                if l == 2:
+                    for i in range(1, ilith+1):
+                        if m == 0:
+                            b4[0, m, i] = 2. / 3. / np.sqrt(5.) * \
+                                omega**2 * radius[i] * \
+                                hlm[i].coeffs[0, l, m] * p402020
+                        elif m == 1:
+                            b4[0, m, i] = 2. / 3. / np.sqrt(5.) * \
+                                omega**2 * radius[i] * \
+                                hlm[i].coeffs[0, l, m] * p412021
+                        elif m == 2:
+                            b4[0, m, i] = 2. / 3. / np.sqrt(5.) * \
+                                omega**2 * radius[i] * \
+                                hlm[i].coeffs[0, l, m] * p422022
+
+                # --- do sine term ---
+
+                b = np.zeros(ilith+2)
+                if m != 0:
+                    # Add contributions from degree 2 relief to degree 4.
+                    if l == 4 and m <= 2:
+                        b[1:ilith+1] = b4[1, m, 1:ilith+1]
+                    b[ilith+1] = gm * potential.coeffs[1, l, m] / r0
+
+                    # calculate delta
+                    if k > 0:
+                        d = np.zeros(ilith+2)
+                        for i in range(1, ilith+1):
+                            for j in range(1, ilith+1):
+                                if j <= i:
+                                    d[i] -= dcplus[j, 1, l, m] * gm * \
+                                        radius[j]**l / radius[i]**(l+1)
+                                else:
+                                    d[i] -= dcminus[j, 1, l, m] * gm * \
+                                        radius[i]**l / radius[j]**(l+1)
+                        for j in range(1, ilith+1):
+                            d[ilith+1] -= dcplus[j, 1, l, m] * gm * \
+                                        radius[j]**l / r0**(l+1)
+
+                    # solve the linear equation A h = b
+                    atemp = a.copy()
+                    if tides:
+                        for i in range(1, ilith+1):
+                            atemp[i, i] += atides[1, i]
+
+                    btemp = b.copy()
+                    if k > 0:
+                        btemp += d
+
+                    # note that the zero index is not used
+                    lu, piv, x, info = lapack.dgesv(atemp[1:, 1:], btemp[1:])
+                    if info != 0:
+                        raise("lapack.dgesv did not exit properly: {:d}", info)
+                    for i in range(1, ilith+1):
+                        hlm[i].coeffs[1, l, m] = x[i-1]
+
+                    # calculate b4 contribution
+                    if l == 2:
+                        for i in range(1, ilith+1):
+                            if m == 1:
+                                b4[1, m, i] = 2. / 3. / np.sqrt(5.) * \
+                                    omega**2 * radius[i] * \
+                                    hlm[i].coeffs[1, l, m] * p412021
+                            elif m == 2:
+                                b4[1, m, i] = 2. / 3. / np.sqrt(5.) * \
+                                    omega**2 * radius[i] * \
+                                    hlm[i].coeffs[1, l, m] * p422022
+
+    # Calculate potential at r0 resulting from all interfaces below and
+    # including ilith
+
+    coeffs = np.zeros((2, lmax+1, lmax+1))
+    for i in range(1, ilith+1):
+        if finiteamplitude is True:
+            grid = hlm[i].expand(grid='DH1', lmax=lmaxgrid, lmax_calc=lmax)
+            clm, r = pyshtools.gravmag.CilmPlusDH(grid.data, nmax, gm/g,
+                                                  drho[i], lmax=lmax)
+            for l in range(2, lmax+1):
+                coeffs[:, l, :l+1] += clm[:, l, :l+1] * (radius[i] / r0)**l
+        else:
+            for l in range(2, lmax+1):
+                coeffs[:, l, :l+1] += hlm[i].coeffs[:, l, :l+1] * 4. * \
+                    np.pi * drho[i] * radius[i]**2 * (radius[i] / r0)**l * \
+                    g / gm / (2. * l + 1.)
+
+    clm_hydro = pyshtools.SHCoeffs.from_array(coeffs, normalization='4pi',
+                                              csphase=1)
+    clm_hydro.r0 = r0
+    clm_hydro.gm = gm
+
+    return hlm, clm_hydro, mass_model
